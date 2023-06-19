d21bfd221e7803c4dca48e1b196ec57a364092da
Bruno Nascimento <sunr00t@gmail.com>
2023-06-19 18:38:47 -0300

    api form

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,12 +1,54 @@
+import React from 'react';
+import { useState } from 'react';
+
 export default function Form() {
+  const [variance, setVariance] = useState(0);
+  const [curtosis, setCurtosis] = useState(0);
+  const [skewness, setSkewness] = useState(0);
+  const [entropy, setEntropy] = useState(0);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const payload = { 
+      variance,
+      curtosis,
+      skewness,
+      entropy
+    }
+    console.log(payload)
+    
+    try {
+      // Envie os dados do formulário para a API usando fetch
+      const response = await fetch('/api/svm', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload),
+      });
+
+      if (response.ok) {
+        console.log(response.json);
+        ;
+      } else {
+        console.log('else');
+        
+      }
+    } catch (error) {
+      console.log('error');
+    }
+  };
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit}>
       <div>
         <div className="mt-8 max-w-md">
           <div className="grid grid-cols-1 gap-6">
             <label className="block">
               <span className="text-white-700">Variance</span>
               <input
+                value={variance}
+                onChange={e => setVariance(e.target.value)}
                 type="number"
                 className="mt-3
                 block
@@ -16,7 +58,6 @@ export default function Form() {
                 hover: bg-green-800 font-medium transition
                 border-transparent
                 border border-green-600"
-                placeholder=""
               />
             </label>
           </div>
@@ -24,7 +65,10 @@ export default function Form() {
             <label className="block">
               <span className="text-white-700">Curtosis</span>
               <input
+                value={curtosis}
+                onChange={e => setCurtosis(e.target.value)}
                 type="number"
+                name="curtosis"
                 className="mt-3
                 block
                 w-full
@@ -33,7 +77,6 @@ export default function Form() {
                 hover: bg-green-800 font-medium transition
                 border-transparent
                 border border-green-600"
-                placeholder=""
               />
             </label>
           </div>
@@ -41,7 +84,10 @@ export default function Form() {
             <label className="block">
               <span className="text-white-700">Skewness</span>
               <input
+                value={skewness}
+                onChange={e => setSkewness(e.target.value)}
                 type="number"
+                name="skewness"
                 className="mt-3
                 block
                 w-full
@@ -50,7 +96,6 @@ export default function Form() {
                 hover: bg-green-800 font-medium transition
                 border-transparent
                 border border-green-600"
-                placeholder=""
               />
             </label>
           </div>
@@ -58,7 +103,10 @@ export default function Form() {
             <label className="block">
               <span className="text-white-700">Entropy</span>
               <input
+                value={entropy}
+                onChange={e => setEntropy(e.target.value)}
                 type="number"
+                name="entropy"
                 className="mt-3
                 block
                 w-full
@@ -67,12 +115,21 @@ export default function Form() {
                 hover: bg-green-800 font-medium transition
                 border-transparent
                 border border-green-600"
-                placeholder=""
               />
             </label>
           </div>
           <div className="mt-4">
-            <button className="MT-2 bg-green-800 hover:bg-green-700 text-white font-bold py-2 px-4 rounded border border-green-600">
+            <button
+              type="submit"
+              className="mt-3
+              bg-green-800 
+              hover:bg-green-700 
+              text-white 
+              font-bold 
+              py-2 px-4 
+              rounded border 
+              border-green-600"
+            >
               Classificar
             </button>
           </div>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true
-};
+  reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/svm",
+        destination: "http://localhost:5000/validate",
+      },
+    ];
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/api/svm",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: 'Referrer-Policy', value: "strict-origin-when-cross-origin"},
+          { key: "Access-Control-Allow-Methods", value: "GET,POST",
+          },
+        ],
+      },
+    ]
+  },
+}
